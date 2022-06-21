e0ea505aa4f20f66c16f7c09705a72968b94dff5
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-06-22 01:17:19 +0530

    Added: Displaying Welcome Message

---
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,12 @@
+package com.bridgelabz;
+
+/*
+ * @author: Nikhil Deshmukh
+ * Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+ */
+public class EmployeeWage {
+    // Main method
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}

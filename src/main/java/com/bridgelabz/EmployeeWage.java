570c4ed093b088e0af3e776d621f7c5af0de496d
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-06-22 02:04:58 +0530

    Added: Part time employee and wage

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -1,30 +1,35 @@
 package com.bridgelabz;
 /*
  * @author: Nikhil Deshmukh
- * Calculate Daily Employee Wage
+ * Add Part time Employee & Wage
  * - Assume wage per Hour is 20, - Assume Full Day Hour is 8
+ * - Assume Part time Hour is 8
  */
 public class EmployeeWage {
     // Main method
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        // Variables
-        int IS_FULL_TIME = 1;
+        // Constants
+        int IS_PART_TIME = 1;
+        int IS_FULL_TIME = 2;
         int EMP_RATE_PER_HOUR = 20;
         // Local variables
         int empHrs = 0;
         int empWage = 0;
-        // Random function to generate number 1 or 0
-        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
-        // Checking Employee is present or not
-        if (empCheck == IS_FULL_TIME) {
+        // Random function to generate number
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 3);
+        // Checking Employee is Part time or full time present
+        if (empCheck == IS_PART_TIME) {
+            empHrs = 4;
+            System.out.println("Employee is Part Time Present");
+        } else if (empCheck == IS_FULL_TIME) {
             empHrs = 8;
-            System.out.println("Employee is Present");
+            System.out.println("Employee is Full Time Present");
         } else {
             empHrs = 0;
             System.out.println("Employee is Absent");
         }
-        // Calculating Employee Wage
+        // Calculating Daily Employee Wage
         empWage = empHrs * EMP_RATE_PER_HOUR;
         System.out.println("Daily Employee Wage: " + empWage);
     }

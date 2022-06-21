164190c0afae451c9f19c3a3970005490b71859d
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-06-22 01:31:03 +0530

    Added: Checking Employee is Present or Absent

--- src/main/java/com/bridgelabz/EmployeeWage.java
+++ src/main/java/com/bridgelabz/EmployeeWage.java
@@ -2,11 +2,23 @@ package com.bridgelabz;
 
 /*
  * @author: Nikhil Deshmukh
- * Start with Displaying Welcome to Employee Wage Computation Program on Master Branch
+ * Check Employee is Present or Absent
+ * -Use ((RANDOM)) for Attendance Check
  */
 public class EmployeeWage {
     // Main method
     public static void main(String[] args) {
+        // Displaying welcome message
         System.out.println("Welcome to Employee Wage Computation Program");
+        // Variable
+        int isPresent = 1;
+        // Random function to generate number
+        int empCheck = (int) (Math.floor(Math.random() * 10) % 2);
+        // Checking employee is present or not
+        if (empCheck == isPresent) {
+            System.out.println("Employee is Present");
+        } else {
+            System.out.println("Employee is Absent");
+        }
     }
 }
